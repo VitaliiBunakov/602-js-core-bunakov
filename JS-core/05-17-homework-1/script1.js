2e4c238fd663caf24e235c3b133371842a17f718
VitaliiBunakov <boonakow@gmail.com>
2021-05-17 20:29:31 +0300

    added dir homework1 done homework1

---
+++ JS-core/05-17-homework-1/script1.js
@@ -0,0 +1,84 @@
+
+//task 2
+
+// console.log('Bunakov Vitalii');
+
+//task 3
+
+//  let a= 3,b =4;
+//
+// let c = a + b;
+// console.log(c);
+//
+// console.log(a += b)
+//
+// console.log(a );
+// console.log(b);
+// console.log(c);
+
+
+//task 4
+
+// let vvaribless = {
+//     Stringg : 'string',
+//     Numberr : 42,
+//     Booleann : true,
+//     Undefined :undefined ,
+//     Nullll : null
+//
+// };
+
+
+//task 5
+
+// let isAdult  = confirm('Skiko bulba po znyzhci ide, znajesh?');
+
+//task 6
+
+function six()  {
+    let name = 'Vitalii';
+    let surname = 'Bunakov';
+    let group = '602-js-core';
+    let birthYear = 1990;
+    let married = false;
+    const  nuuuuull = null;
+    const  undef = undefined;
+
+    console.log(typeof (birthYear));
+    console.log(typeof (married));
+    console.log(typeof (name));
+    console.log(typeof (nuuuuull));
+    console.log(typeof (undef));
+
+}
+// six();
+
+//task 7
+
+function seven (){
+    let name = prompt('Your name is ...');
+    let pass = prompt('Your pass is ...');
+    let email = prompt('Your email is ...');
+
+    alert( `Your name is ${name} and your pass is ${pass} so,  now i can get into your email ${email}`)
+
+}
+
+// seven();
+
+//task 8
+function eight(){
+    let secInMin = 60;
+    let secInHour = secInMin * 60;
+    let secInDay = secInHour * 24;
+    let secInMonth = secInDay * 30;
+
+    console.log(secInMin);
+    console.log(secInHour);
+    console.log(secInDay);
+    console.log(secInMonth);
+}
+
+// eight();
+
+
