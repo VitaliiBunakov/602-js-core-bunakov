9f6b6da48a8219859f07f41fa8923e718be104e7
VitaliiBunakov <boonakow@gmail.com>
2021-05-24 19:00:53 +0300

    added dir 05-24-practice

--- JS-core/05-17-homework-1/script1.js
+++ JS-core/05-17-homework-1/script1.js
@@ -83,4 +83,3 @@ function eight(){
 
 // eight();
 
-
---
+++ JS-core/05-24-practice/script.js
@@ -0,0 +1,31 @@
+const arr1 = [2, 3, 4, 5];
+
+
+
+//task 1
+let mult = 1;
+for (let i of arr1){
+    mult *= i;
+}
+console.log(mult);
+
+mult = 1;
+let count1 = 0;
+while (count1 < arr1.length){
+    mult *= arr1[count1];
+    count1++
+}
+console.log(mult);
+
+
+//task 2
+
+
+
+
+//task
+//task
+//task
+//task
+//task
+//task
