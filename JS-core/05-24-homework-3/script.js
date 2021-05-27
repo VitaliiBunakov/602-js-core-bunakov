67782ea6f2720e712f5df2404be3123c3a3f8250
VitaliiBunakov <boonakow@gmail.com>
2021-05-27 13:44:38 +0300

    backup

---
+++ JS-core/05-24-homework-3/script.js
@@ -0,0 +1,79 @@
+const arr1 = [2, 3, 4, 5];
+
+
+//task 1
+// let mult = 1;
+// for (let i of arr1){
+//     mult *= i;
+// }
+// console.log(mult);
+//
+// mult = 1;
+// let count1 = 0;
+// while (count1 < arr1.length){
+//     mult *= arr1[count1];
+//     count1++
+// }
+// console.log('=====');
+// console.log(mult);
+
+//task 2
+// for (let i = 0; i<=15;i++) {
+//     if(i%2){
+//         console.log(`${i} is odd`);
+//     }else {
+//         console.log(`${i} is even`);
+//     }
+// }
+
+//task  3
+// function randArray( k){
+//     let arr =[];
+//     for ( let i= 1; i < k; i++){
+//         arr.push(Math.floor(Math.random() * (500 - 1) + 1))
+//     }
+//     return arr;
+// }
+// let outArr = randArray(5);
+// console.log(`Random array is  ${outArr}`);
+
+//task  4
+// function raiseToDegree(a,b){
+//     let c = a;
+//     for (let i=1; i< b; i++){
+//         c*=a;
+//     }
+//     return c;
+// }
+// console.log(raiseToDegree(3, 2));
+// console.log(raiseToDegree(3, 3));
+// console.log(raiseToDegree(3, 4));
+// console.log(raiseToDegree(3, 5));
+
+
+//task  5
+// function findMin() {
+//     let min = arguments[0];
+//     if (arguments.length < 1) {
+//         return 'need some args'
+//     }
+//     for (let i = 0; i < arguments.length; i++) {
+//         if (min > arguments[i]) {
+//             min = arguments[i]
+//         }
+//     }
+//     return min;
+// }
+//
+// const xxx = findMin(-2,5,-100,5,78,2,4);
+// console.log(xxx);
+
+//task  6
+function  findUnique(arr){
+
+}
+
+//task  7
+
+
+//task  8
