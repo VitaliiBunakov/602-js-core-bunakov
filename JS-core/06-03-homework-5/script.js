b3e539ad2f1cc7f9babc0832fef51bf89e277241
VitaliiBunakov <boonakow@gmail.com>
2021-06-08 01:47:33 +0300

    homework 5  done:  1  2  In progress  3  todo  4

--- JS-core/06-03-homework-5/script.js
+++ JS-core/06-03-homework-5/script.js
@@ -61,18 +61,37 @@
 /**
  * Створіть клас Person, в якого конструктор приймає параметри name і surname, а також міститься метод showFullName(),
  * який виводить ім’я і прізвище особи.
+ *
  * Від класу Person наслідується клас Student, конструктор якого крім name і surname,
  * приймає параметр year (рік вступу до університету).
  * В класі Student необхідно перевизначити метод showFullName(midleName), щоб виводилося не лише ім’я,
  * прізвище, але і по-батькові (midleName) студента.
+ *
  * Також в класі Student необхідно реалізувати метод showCourse(), який виводитиме поточний курс студента (від 1 до 6).
  * Значення курсу визначатиметься як різниця поточного року (визначити самостійно) і року вступу до ВУЗу year.
  * Приклад результату:
+ *
  * let stud1 = new Student("Petro", "Petrenko", 2015);
  * console.log(stud1.showFullName("Petrovych")); // Petrenko Petro Petrovych
  * console.log("Current course: " + stud1.showCourse()); //Current course: 6
 
  */
+class Person{
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    showFullName(){
+        return this.name + ' ' + this.surname;
+    }
+
+}
+class Student{
+    constructor() {
+        super name surname)
+    }
+}
 
 
 
